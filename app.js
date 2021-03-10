8cba00c90e9afac8d7eabccb34b3f09b131d3281
HungXingYu <clarehung0406@gmail.com>
2021-03-10 08:43:54 +0800

    Add express-validator and modify app.post()

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 /**Include express and express-handlebars */
 const express = require('express')
 const exphbs = require('express-handlebars')
+const { body , validationResult  } = require('express-validator')
 /**Define server related variables */
 const app = express()
 const port = 3000
@@ -33,37 +34,71 @@ app.use(bodyParser.urlencoded({ extended: true }))
 //#region  Setting routes
 /**Index 頁面資料取得與渲染 */
 app.get('/', (req, res) => {
-  res.render('index',{restaurants:restaurantList.results})
+  res.render('index', { restaurants: restaurantList.results })
 })
 /**Show 頁面資料取得與渲染 */
-app.get('/restaurants/:restaurant_id', (req,res)=>{
+app.get('/restaurants/:restaurant_id', (req, res) => {
   const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
-  
-  res.render('show' , {restaurant})
+
+  res.render('show', { restaurant })
 })
 /**Search 資料取得與渲染*/
-app.get('/search' ,(req,res) =>{
-  const keyword= req.query.keyword
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
   let restaurants
-  const searchResult = restaurantList.results.filter(restaurant => restaurant.name.toLowerCase().includes(keyword.toLowerCase())) 
-  searchResult.length !== 0 
-   ? restaurants = searchResult
-   : restaurants = restaurantList.results.filter(restaurant => restaurant.category.toLowerCase().includes(keyword.toLowerCase())) 
-  
-   res.render('index',{restaurants , keyword})
+  const searchResult = restaurantList.results.filter(restaurant => restaurant.name.toLowerCase().includes(keyword.toLowerCase()))
+  searchResult.length !== 0
+    ? restaurants = searchResult
+    : restaurants = restaurantList.results.filter(restaurant => restaurant.category.toLowerCase().includes(keyword.toLowerCase()))
+
+  res.render('index', { restaurants, keyword })
 })
 /**Add 頁面資料取得與渲染 */
-app.get('/add' , (req,res)=>{
+app.get('/add', (req, res) => {
   res.render('add')
 })
 /**Post 資料取得與渲染 */
-app.post('/' , (req , res) => {  
-  const newRestaurant = req.body
-  const id = parseInt(restaurantList.total)+1
-  newRestaurant.id = id  
-  restaurantList.results.push(newRestaurant)
-  res.render('index',{restaurants: restaurantList.results})
-  insertRestaurantJson(fs , newRestaurant)
+// app.post('/add', (req, res) => {
+//   const newRestaurant = req.body
+//   const checkEmpty = Object.values(newRestaurant).some(function(item , index , array){
+//     return item.trim().length === 0
+//   })
+  
+//   if(checkEmpty){
+//     console.log('餐廳資料不得輸入空白，請重新輸入')
+//   }
+//   else
+//   {
+//     const id = parseInt(restaurantList.total) + 1
+//     newRestaurant.id = id
+//     restaurantList.results.push(newRestaurant)
+//     insertRestaurantJson(fs, newRestaurant)
+//     res.redirect('/')    
+//   }
+// })
+/**Post 資料取得與渲染 */
+app.post('/add' , [body('name').trim().isLength({ min: 1 }).withMessage('中文名不得僅輸入空白!'),
+                             body('name_en').trim().isLength({ min: 1 }).withMessage('英文名不得僅輸入空白!'),
+                             body('category').trim().isLength({ min: 1 }).withMessage('類別不得僅輸入空白!'),
+                             body('image').trim().isLength({ min: 1 }).withMessage('圖片不得僅輸入空白!'),
+                             body('image').trim().isURL().withMessage('圖片必須輸入符合網址格式的網址!'),
+                             body('location').trim().isLength({ min: 1 }).withMessage('地址不得僅輸入空白!'),
+                             body('phone').trim().isLength({ min: 1 }).withMessage('電話不得僅輸入空白!'),
+                             body('google_map').trim().isLength({ min: 1 }).withMessage('Google Map不得僅輸入空白!'),
+                             body('google_map').trim().isURL().withMessage('Google Map必須輸入符合網址格式的網址!'),
+                             body('rating').trim().isLength({ min: 1 }).withMessage('評分不得僅輸入空白!'),
+                             body('description').trim().isLength({ min: 1 }).withMessage('簡介不得僅輸入空白!'),],(req,res)=>{ 
+    const newRestaurant = req.body
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const errorMessages = errors.array()
+      return res.render('add', {errorMessages, newRestaurant})
+    }
+    const id = parseInt(restaurantList.total) + 1
+    newRestaurant.id = id
+    restaurantList.results.push(newRestaurant)
+    insertRestaurantJson(fs, newRestaurant)
+    res.redirect('/') 
 })
 //#endregion
 
@@ -72,3 +107,4 @@ app.listen(port, () => {
   console.log(`Express app listening on port ${port}.`)
 })
 //#endregion
+
